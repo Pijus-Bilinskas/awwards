cb6df72acb4aecdedcc3c367031c710b88e1f43b
Pijus Bilinskas <pijus.bilins@gmail.com>
2025-01-27 16:07:40 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <main>
+      <h1 className='text-violet-300 text-5xl font-general font-bold'>
+      Welcome To Awwards
+      </h1>
+    </main>
+  )
+}
+
+export default App
